f6997cdf2ce9e43f174299f52e022efed157370a
Gunjan <gunnugupta1999@gmail.com>
2024-06-05 12:44:21 +0530

    delete vnet

--- main.tf
+++ main.tf
@@ -21,9 +21,3 @@ resource "azurerm_resource_group" "rg" {
   name     = "rg-gunnugupta1999" # change to your rg name
   location = var.location
 }
-resource "azurerm_virtual_network" "vnet" {
-  name                = "${var.prefix}-vnet"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  address_space       = var.vnetAdressSpace
-}
